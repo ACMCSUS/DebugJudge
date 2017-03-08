b53044c0239365bbbba0ed2866032167c292ab51
MatthewMerrill <MattMerr47@gmail.com>
2017-03-08 10:56:04 -0800

    Angular2 Setup

--- debugjudgeapp/src/app/app.module.ts
+++ debugjudgeapp/src/app/app.module.ts
@@ -2,17 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team.component';
+import { JudgeComponent } from "./judge.component";
+import { ScoreboardComponent } from "./scoreboard.component";
+
+const appRoutes: Routes = [
+  { path: 'team', component: TeamComponent },
+  { path: 'judge', component: JudgeComponent },
+  { path: 'scoreboard', component: ScoreboardComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent,
+    JudgeComponent,
+    ScoreboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
