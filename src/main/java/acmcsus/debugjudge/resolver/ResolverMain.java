ce2531ac164c81e7fc96c2fc8bad10f7813528f6
Matthew Merrill <MattMerr47@gmail.com>
2018-04-24 20:26:08 -0700

    remove bad thing

--- src/main/java/acmcsus/debugjudge/resolver/ResolverMain.java
+++ src/main/java/acmcsus/debugjudge/resolver/ResolverMain.java
@@ -1,11 +1,9 @@
 package acmcsus.debugjudge.resolver;
 
-import acmcsus.debugjudge.proto.Algorithmic;
-
 public class ResolverMain {
 
   public static void main(String[] args) {
-    ResolutionStepper stepper = new ResolutionStepper();
+//    ResolutionStepper stepper = new ResolutionStepper();
 
 
 
